d19af6993bf7534c769bd84257d229d4f7e4f2f1
“icarlosquintero” <icarlosquintero@hotmail.com>
2022-12-22 00:01:02 -0300

    primer Entregable V2

--- src/App.js
+++ src/App.js
@@ -9,15 +9,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
+        <div>
+          <Card />
+          <Button />
+        </div>
         <div>
           <Card />
           <Button />
